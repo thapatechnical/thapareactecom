25d5de0fdcf561567f40f3040cdc49c158dc9f97
thapatechnical <vbthapa55@gmail.com>
2022-10-05 16:43:03 +0530

    axios to fecth api data

--- src/context/productcontex.js
+++ src/context/productcontex.js
@@ -1,12 +1,29 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
+import axios from "axios";
 
 const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
 const AppProvider = ({ children }) => {
+  const getProducts = async (url) => {
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      console.log(
+        "🚀 ~ file: productcontex.js ~ line 18 ~ getProducts ~ products",
+        products
+      );
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+
   return (
-    <AppContext.Provider value={{ myName: "vinod thapa" }}>
-      {children}
-    </AppContext.Provider>
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   );
 };
 
