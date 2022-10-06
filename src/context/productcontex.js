a3a6851b46d7d3f921f6488351cffa7291c33d7e
thapatechnical <vbthapa55@gmail.com>
2022-10-06 12:21:18 +0530

    added product reducer section

--- src/context/productcontex.js
+++ src/context/productcontex.js
@@ -1,21 +1,30 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import axios from "axios";
+import reducer from "../reducer/productReducer";
 
 const AppContext = createContext();
 
 const API = "https://api.pujakaitem.com/api/products";
 
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
+};
+
 const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const getProducts = async (url) => {
     dispatch({ type: "SET_LOADING" });
     try {
       const res = await axios.get(url);
       const products = await res.data;
-      console.log(
-        "🚀 ~ file: productcontex.js ~ line 18 ~ getProducts ~ products",
-        products
-      );
-    } catch (error) {}
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch (error) {
+      dispatch({ type: "API_ERROR" });
+    }
   };
 
   useEffect(() => {
