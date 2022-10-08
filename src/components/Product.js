b92673232c2392c259c3704dca956c01379ac086
thapatechnical <vbthapa55@gmail.com>
2022-10-08 16:00:17 +0530

    format the price for indian currency updated

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import FormatPrice from "../Helpers/FormatPrice";
 
 const Product = (curElem) => {
   const { id, name, image, price, category } = curElem;
@@ -14,7 +15,7 @@ const Product = (curElem) => {
         <div className="card-data">
           <div className="card-data-flex">
             <h3>{name}</h3>
-            <p className="card-data--price">{price}</p>
+            <p className="card-data--price">{<FormatPrice price={price} />}</p>
           </div>
         </div>
       </div>
