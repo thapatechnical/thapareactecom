30c5d381db0d0cf2a4d3bb7fe85f55d976004659
Rohit <76204852+RohitChoudhary461771@users.noreply.github.com>
2023-09-20 17:32:31 +0530

    Update Header.js
    Sir you forget to add className in image tag.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <MainHeader>
       <NavLink to="/">
-        <img src="./images/logo.png" alt="my logo img" />
+        <img src="./images/logo.png" alt="my logo img className="logo" />
       </NavLink>
       <Nav />
     </MainHeader>
