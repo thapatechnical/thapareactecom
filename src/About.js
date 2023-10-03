c371bbe585da772005648779daaa568501697a91
Sarthak Das <sarthak.das@cimpress.com>
2023-10-03 10:48:44 +0530

    test

--- src/About.js
+++ src/About.js
@@ -2,6 +2,7 @@ import HeroSection from "./components/HeroSection";
 import { useProductContext } from "./context/productcontex";
 import { AnalyticsBrowser } from "@segment/analytics-next";
 const writeKey = process.env.write_key;
+console.log("Write key ", writeKey);
 const analytics = AnalyticsBrowser.load({ writeKey: writeKey });
 
 const About = () => {
