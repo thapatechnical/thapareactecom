2d585c57c2e38c88f0d794168949937686078cd4
Sarthak Das <sarthak.das@cimpress.com>
2023-11-22 13:56:32 +0530

    Hubspot-form test

--- src/Contact.js
+++ src/Contact.js
@@ -95,7 +95,7 @@ const Contact = () => {
       <div className="container">
         <div className="contact-form">
           <form
-            action="https://formspree.io/f/xrgwozqz"
+            // action="https://formspree.io/f/xrgwozqz"
             method="POST"
             className="contact-inputs">
             <input
