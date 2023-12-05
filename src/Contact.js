ba80c8a3d4fbc72e3cf081bf71b210bdaeb04ebf
Sarthak Das <sarthak.das@cimpress.com>
2023-12-05 16:36:53 +0530

    Discard change

--- src/Contact.js
+++ src/Contact.js
@@ -95,41 +95,9 @@ const Contact = () => {
       <h2 className="common-heading">Contact page</h2>
 
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.6742646941825!2d77.59173997518089!3d13.056393987266778!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae17e0a2ceb611%3A0x6b202b434be4083c!2sCimpress%20Bangalore!5e0!3m2!1sen!2sin!4v1695814999238!5m2!1sen!2sin" width="100%" height="450" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-      <form 
-            // action="https://formspree.io/f/xrgwozqz"
-            // method="POST"
-            // onSubmit={handleSubmit}
-            className="contact-inputs">
-            <input
-              type="text"
-              placeholder="username"
-              name="username"
-              // value={isAuthenticated ? user.name : ""}
-              required
-              autoComplete="off"
-            />
+      
 
-            <input
-              type="email"
-              name="Email"
-              placeholder="Email"
-              autoComplete="off"
-              // value={isAuthenticated ? user.email : ""}
-              required
-            />
-
-            <textarea
-              name="Message"
-              cols="30"
-              rows="10"
-              required
-              autoComplete="off"
-              placeholder="Enter you message"></textarea>
-
-            <input type="submit" value="send" />
-          </form>
-
-      {/* <div className="container">
+      <div className="container">
         <div className="contact-form">
           <form 
             // action="https://formspree.io/f/xrgwozqz"
@@ -165,7 +133,7 @@ const Contact = () => {
             <input type="submit" value="send" />
           </form>
         </div>
-      </div> */}
+      </div>
       
       {/* Embed HubSpot Form
       <div className="container">
